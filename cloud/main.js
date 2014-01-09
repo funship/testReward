3df93f4f58806cf34750274a2afbe020b92fb1a5
funship <funship@funship.org>
2014-01-09 17:21:05 +0800

    fix code random

--- cloud/main.js
+++ cloud/main.js
@@ -15,7 +15,7 @@ AV.Cloud.define("createCode", function(request, response) {
    var gameScore = new GameScore();
    var randomCode = Math.random()*10+1;
    gameScore.save({
-    code: "1338",
+    code: randomCode,
     rewardType: "1234sdf"    
     }, {
     success: function(gameScore) {
