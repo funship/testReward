339015224f1216b87dd3d86785df1208132bc495
funship <funship@funship.org>
2014-01-09 14:01:29 +0800

    add a function
    add a function

--- cloud/main.js
+++ cloud/main.js
@@ -3,3 +3,8 @@
 AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+AV.Cloud.define("good", function(request, response) {
+  var str = request.params.person1 + "is good" + request.params.person2 + "is also good";
+  response.success(str);
+});
