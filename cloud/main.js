382c93ebf2d70c911391abd5701d9b7244b5d42a
funship <funship@funship.org>
2014-01-09 14:31:45 +0800

    have a try
    have a try

--- cloud/main.js
+++ cloud/main.js
@@ -5,6 +5,6 @@ AV.Cloud.define("hello", function(request, response) {
 });
 
 AV.Cloud.define("good", function(request, response) {
-  //var str = request.params.person1 + "is good" + request.params.person2 + "is also good";
-  response.success("is this wrong");
+  var str = request.params.person1 + "is good" + request.params.person2 + "is also good";
+  response.success(str);
 });
