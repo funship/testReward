43e5b29163d1a99cddc71f15eb600a43b8c91550
funship <funship@funship.org>
2014-01-10 11:33:47 +0800

    let try it

--- cloud/main.js
+++ cloud/main.js
@@ -74,8 +74,8 @@ function getRewardCode(GameReward,totalNumber)
 */
 function getRewardCode(tableName,totalNumber)
 {
-   var GameRward = AV.Object.extend("Reward");
-   var gameRward = new GameRward();
+   //var GameRward = AV.Object.extend("Reward");
+   //var gameRward = new GameRward();
    var codeIndex = 0;
     response.success("create code ok");
 	
@@ -131,8 +131,8 @@ AV.Cloud.define("createCode", function(request, response) {
    var codeNumber = request.params.number;
    if(str == "funship@funship.org.123")
    {
-       response.success("create code ok");
-       //getRewardCode(tableName,codeNumber);
+       //response.success("create code ok");
+       getRewardCode(tableName,codeNumber);
    }
    
    
