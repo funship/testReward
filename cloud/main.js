69d4326f049b855c563db74057e7598dfbe036e3
funship <funship@funship.org>
2014-01-10 11:32:09 +0800

    is it still ok
    is it still ok

--- cloud/main.js
+++ cloud/main.js
@@ -131,8 +131,8 @@ AV.Cloud.define("createCode", function(request, response) {
    var codeNumber = request.params.number;
    if(str == "funship@funship.org.123")
    {
-       //response.success("create code ok");
-       getRewardCode(tableName,codeNumber);
+       response.success("create code ok");
+       //getRewardCode(tableName,codeNumber);
    }
    
    
