061bc27e9adb66049b4ab57c02ebe84e2fc180b4
funship <funship@funship.org>
2014-01-10 10:52:25 +0800

    fix token
    fix token

--- cloud/main.js
+++ cloud/main.js
@@ -7,69 +7,69 @@ AV.Cloud.define("good", function(request, response) {
 
 function checkDulicateCode(GameReward,code)
 {
-	var isDulicateCode = false;
-	var query = new AV.Query(GameRward);
-		query.equalTo("code", request.params.code);
-		
-		query.find({
-		success: function(results) {
-		if( results.length > 0 )
-		{
-			isDulicateCode = true;
-		}
-	  //
-		},
-		error: function() {
-			response.error("can't fing code");
-		}
-	return isDulicateCode;
+    var isDulicateCode = false;
+    var query = new AV.Query(GameRward);
+        query.equalTo("code", request.params.code);
+        
+        query.find({
+        success: function(results) {
+        if( results.length > 0 )
+        {
+         isDulicateCode = true;
+        }
+         //
+        },
+        error: function() {
+             response.error("can't fing code");
+        }
+    return isDulicateCode;
 }
 
 
 
 function getRewardCode(GameReward,totalNumber)
 {
-	var gameRward = new GameRward();
-	var codeIndex = 0;
+    var gameRward = new GameRward();
+    var codeIndex = 0;
     while(codeIndex < totalNumber)
-	{
-		var codeNumber=""; 
-		for(var i=0;i<15;i++) 
-		{ 
-			codeNumber+=Math.floor(Math.random()*10); 
-		} 
-		
-		var rewardRandom = Math.floor(Math.random() * 100);
-		var rewardNum = 1;
-		if(rewardRandom < 90)
-		{
-			rewardNum = 1;
-		}else if(rewardRandom < 98)
-		{
-			rewardNum = 2;
-		}
-		else
-		{
-			rewardNum = 3;
-		}
-		
-		
-		if(!checkDulicateCode(GameReward,codeNumber))		
-		{
-			gameScore.save({
-			code: codeNumber,
-			rewardType: rewardNum    
-			}, {
-			success: function(gameScore) {
-			codeIndex = codeIndex + 1;
-			},
-			error: function(gameScore, error) {
-			// The save failed.
-			// error is a AV.Error with an error code and description.
-			}
-			});
-		}		
-	}
+    {
+        var codeNumber=""; 
+        for(var i=0;i<15;i++) 
+        { 
+             codeNumber+=Math.floor(Math.random()*10); 
+        } 
+        
+        var rewardRandom = Math.floor(Math.random() * 100);
+        var rewardNum = 1;
+        if(rewardRandom < 90)
+        {
+          rewardNum = 1;
+        }else if(rewardRandom < 98)
+        {
+          rewardNum = 2;
+        }
+        else
+        {
+          rewardNum = 3;
+        }
+   
+ 
+        if(!checkDulicateCode(GameReward,codeNumber))		
+        {
+            gameScore.save({
+            code: codeNumber,
+            rewardType: rewardNum    
+            }, {
+            success: function(gameScore) {
+            codeIndex = codeIndex + 1;
+            },
+            error: function(gameScore, error) {
+            // The save failed.
+            // error is a AV.Error with an error code and description.
+            }
+           });
+        } 
+    }
 }
 
 AV.Cloud.define("createCode", function(request, response) {
@@ -92,13 +92,13 @@ AV.Cloud.define("getReward", function(request, response) {
   query.equalTo("code", request.params.code);
   query.find({
     success: function(results) {
-	  if( results.length > 0 )
-	  {
-		var object = results[0];
-		response.success(object.get('rewardType'));
-		object.destroy();
-	  }
-	  //
+     if( results.length > 0 )
+     {
+        var object = results[0];
+        response.success(object.get('rewardType'));
+        object.destroy();
+     }
+     //
     },
     error: function() {
       response.error("can't fing code");
